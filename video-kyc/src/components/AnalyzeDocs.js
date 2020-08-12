de3ace889b88a6d1274976d7b982335f558c23d1
Sathish <sat.hariharan@gmail.com>
2020-08-12 22:54:54 +0530

    update the message

--- video-kyc/src/components/AnalyzeDocs.js
+++ video-kyc/src/components/AnalyzeDocs.js
@@ -21,7 +21,7 @@ const videoConstraints = {
 };
 
 const side_data = [
-    {"name":"front", "description":"Show the front of the Photo ID to the camera"},
+    {"name":"front", "description":"Show the photo page of your photo id (Aadhaar or Passport in India) to the camera"},
     {"name":"back", "description":"Show the back of the Photo ID to the camera"}
 ]  
 
